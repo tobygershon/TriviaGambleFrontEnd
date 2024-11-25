4a564b9bb9ec6dcb0356cd3997267bc078f77c65
tobygershon <154568289+tobygershon@users.noreply.github.com>
2024-11-25 18:18:54 -0500

    changed axios baseURL to railway domain backend

--- src/services/BackEndService.tsx
+++ src/services/BackEndService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const localHost = axios.create({
-    baseURL: 'http://127.0.0.1:5000'
+    baseURL: 'http://triviagambleflaskbackend-production.up.railway.app'
 })
 
 // get all unstarted games
